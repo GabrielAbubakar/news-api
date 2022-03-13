db81f7eb796f5129e80fa6fc61b14687cd0e7e0a
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-03-13 17:31:33 +0100

    staggered animations added

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import './NewsItem.css'
+import { motion } from 'framer-motion'
 
-const NewsItem = ({ title, publisher, content, link }) => {
+const NewsItem = ({ title, publisher, content, link, index }) => {
 
     return (
-        <div className='news__item'>
+        <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: .8, delay: index * 0.2 }}
+            className='news__item'>
+
             <h3>{title}</h3>
             <p id='publisher'>{publisher}</p>
             <p id='content'>
                 {content}
             </p>
             <a href={link} target="_blank">Read More</a>
-        </div>
+        </motion.div>
     )
 }
 
